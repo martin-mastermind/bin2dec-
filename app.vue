62a265955bc24638ce2714e57abfcc8784820ce7
Martin <martbelmoaw@gmail.com>
2023-12-09 11:54:11 +0300

    First Commit

---
+++ app.vue
@@ -0,0 +1,15 @@
+<template>
+  <main>
+    <NuxtPage />
+  </main>
+</template>
+
+<script setup lang="ts">
+useHead({
+  title: "Mastermind - Bin2Dec",
+  meta: [{ name: "description", content: "Binary to decimal converter" }],
+  htmlAttrs: {
+    lang: "en",
+  },
+});
+</script>
---
+++ pages/index.vue
@@ -0,0 +1,11 @@
+<template>
+  <div>
+    <NuxtLazyHydrate when-idle>
+      <p @click="console.log('hello!')">Nuxt module playground!</p>
+    </NuxtLazyHydrate>
+  </div>
+</template>
+
+<script lang="ts" setup></script>
+
+<style></style>
